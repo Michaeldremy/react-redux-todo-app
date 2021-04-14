6674ca495d664e39e65fd35c9685bdafcbba4751
michael420 <michael@castus.tv>
2021-04-13 21:35:49 -0700

    Learning Redux tool kit

--- starter/src/components/AddTodoForm.js
+++ starter/src/components/AddTodoForm.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/todoSlice';
 
 const AddTodoForm = () => {
 	const [value, setValue] = useState('');
+	const dispatch = useDispatch();
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		console.log('user entered: ' + value);
+		if (value) {
+			// dispatch our reducer
+			dispatch(
+				addTodo({
+					title: value,
+				})
+			)
+		}
 	};
 
 	return (
